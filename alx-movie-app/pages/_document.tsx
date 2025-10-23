85c165436935364dbc878c7ff29625befe0f2649
Sam-Adk <samsmollett50@gmail.com>
2025-10-23 11:20:27 +0300

    Link manifest.json in _document.tsx for PWA setup

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Link to the Web App Manifest */}
+        <link rel="manifest" href="/manifest.json" />
+        {/* Theme color used by the browser’s UI */}
+        <meta name="theme-color" content="#0070f3" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
